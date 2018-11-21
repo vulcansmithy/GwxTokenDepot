27e2e3b671c9fcc16b341c408373c0d379f7aa23
Ulysses E. Legaspi <ulysses.legaspi@gmail.com>
2018-11-21 11:27:50 +0800

    Setup Versionist API route container.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  api_version(:module => "Api::V1", :header => {:name => "Accept", :value => "application/vnd.gameworks.io; version=1"}, :parameter => {:name => "version", :value => "1"}, :path => {:value => "v1"}, :defaults => {:format => "json"}, :default => true) do
+  end  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
