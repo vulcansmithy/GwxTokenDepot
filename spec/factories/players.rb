2311dff93b659bc5d34de470f076c92247c0c93b
Ulysses E. Legaspi <ulysses.legaspi@gmail.com>
2018-12-03 12:47:47 +0800

    Minor code refactor.

--- spec/factories/players.rb
+++ spec/factories/players.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :player do
     first_name { "MyString" }
-    last_name { "MyString" }
-    email { "MyString" }
+    last_name  { "MyString" }
+    email      { "MyString" }
   end
 end
