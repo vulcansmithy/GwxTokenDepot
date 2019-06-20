00bd6d55bda6fad20e3450cffcd6b4a3a28f88cb
Ulysses E. Legaspi <ulysses.legaspi@gmail.com>
2019-06-20 11:56:45 +0800

    Remove unused codes.

--- spec/models/publisher_spec.rb
+++
@@ -1,8 +0,0 @@
-require "rails_helper"
-
-RSpec.describe Publisher, type: :model do
-  it "should do something" do
-    publisher = create(:publisher)
-    expect(publisher.nil?).to eq false
-  end
-end
