d24eb8433abf7ca4ab990917a71717462cf4f98e
Ulysses E. Legaspi <ulysses.legaspi@gmail.com>
2019-06-20 12:08:03 +0800

    Setup a blank TopUpTransaction controller.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   api_version(:module => "Api::V1", :header => {:name => "Accept", :value => "application/vnd.gameworks.io; version=1"}, :parameter => {:name => "version", :value => "1"}, :path => {:value => "v1"}, :defaults => {:format => "json"}, :default => true) do
+    resources :top_up_transactions
     
     # setup the API endpoints for the TopUpTransactions
     resources :top_up_transactions, :only => [:index, :show, :create]
