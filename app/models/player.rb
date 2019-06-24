5b4922de474cd9d160975c5f10c4af8889958939
Ulysses E. Legaspi <ulysses.legaspi@gmail.com>
2019-06-24 23:21:42 +0800

    Remove unused codes.

--- app/models/player.rb
+++
@@ -1,7 +0,0 @@
-class Player
-  include Mongoid::Document
-  
-  field :first_name, type: String, default: nil
-  field :last_name,  type: String, default: nil
-  field :email,      type: String, default: nil
-end
