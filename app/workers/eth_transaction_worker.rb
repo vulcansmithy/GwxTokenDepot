b5bd051a1eeaa30324f9bb26a31545c25c15ba66
Ulysses E. Legaspi <ulysses.legaspi@gmail.com>
2019-06-25 05:25:55 +0800

    Modify EthTransactionWorker to temporary display what parameters is passed to it.

--- app/workers/eth_transaction_worker.rb
+++ app/workers/eth_transaction_worker.rb
@@ -2,8 +2,13 @@ class EthTransactionWorker
 
   include Sidekiq::Worker
 
-  def perform(*args)
-    # Do something
+  def perform(transaction_id)
+    # @TODO
+    eth_transaction = TopUpTransaction.where(id: transaction_id).first
+    
+    puts "@DEBUG L:#{__LINE__}    transaction_id: #{transaction_id    }"
+    puts "@DEBUG L:#{__LINE__}   eth_transaction: #{ap eth_transaction}"
   end
+
 
 end
