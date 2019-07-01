00aac9a7a41ada4d2c6d0f277264a812fb7e6d8d
Ulysses E. Legaspi <ulysses.legaspi@gmail.com>
2019-07-02 00:55:50 +0800

    Implement assign_receiving_wallet and check_btc_wallet_balance methods.

--- app/services/xem_util_service.rb
+++ app/services/xem_util_service.rb
@@ -3,11 +3,20 @@ class XemUtilService < BaseUtilService
   class XemUtilServiceError < StandardError; end
 
   def assign_receiving_wallet(transaction)
-    # @TODO
+    account = NemService.create_account
+    
+    transaction.top_up_receiving_wallet_address = account[:address ]
+    transaction.top_up_receiving_wallet_pk      = account[:priv_key]
+    
+    transaction.save 
+    
+    return transaction
   end  
   
   def check_btc_wallet_balance(transaction)
-    # @TODO
+    result = NemService.check_balance(transaction.top_up_receiving_wallet_address)
+    
+    return result[:xem]
   end
   
   def get_btc_usd_conversion_rate
