e87ba6d457cdf9e6639031f0f4f32e0a627451a5
Ulysses E. Legaspi <ulysses.legaspi@gmail.com>
2019-07-05 14:08:58 +0800

    Reimplement check_btc_wallet_balance method.

--- app/services/eth_util_service.rb
+++ app/services/eth_util_service.rb
@@ -40,4 +40,27 @@ class EthUtilService < BaseUtilService
     return transaction
   end   
   
+  def check_btc_wallet_balance(transaction)
+
+    begin
+      # call the API endpoint
+      response = HTTParty.get("https://api.blockcypher.com/v1/eth/main/addrs/#{transaction.top_up_receiving_wallet_address}/balance")
+      
+      # make sure the response code is :ok before continuing
+      raise BtcUtilServiceError, "Can't reach API endpoint." unless response.code == 200
+      
+      # parse the returned data
+      result = JSON.parse(response.body)
+
+      # retrieve balance data
+      balance = result["balance"]
+      raise "Was not able to returned JSON data 'balance' "if balance.nil?
+
+    rescue Exception => e
+      raise BtcUtilServiceError, e.message
+    else
+      return BigDecimal(balance)
+    end  
+  end 
+  
 end
