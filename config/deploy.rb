4b0bcad9d7c6bd764d998baf2e320e36b7fc91bd
Ulysses E. Legaspi <ulysses.legaspi@gmail.com>
2019-07-16 12:29:25 +0800

    Deploy To Staging.

---
+++ config/deploy.rb
@@ -0,0 +1,89 @@
+set :rbenv_type, :user
+set :rbenv_ruby, File.read('.ruby-version').strip
+set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
+set :rbenv_map_bins, %w{rake gem bundle ruby rails puma pumactl}
+set :rbenv_roles, :all
+
+
+set :repo_url,        'git@github.com:gameworks-gwx/gwx-token-depot.git'
+set :application,     'gwx-token-depot'
+set :user,            'deployer'
+set :puma_threads,    [4, 16]
+set :puma_workers,    0
+ 
+# Don't change these unless you know what you're doing
+set :pty,             true
+set :use_sudo,        false
+set :deploy_via,      :remote_cache
+set :deploy_to,       "/home/#{fetch(:user)}/apps/#{fetch(:application)}"
+set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
+set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
+set :puma_access_log, "#{shared_path}/log/puma.error.log"
+set :puma_error_log,  "#{shared_path}/log/puma.access.log"
+set :ssh_options,     { forward_agent: true, user: fetch(:user), keys: %w(~/.ssh/id_rsa.pub) }
+set :puma_preload_app, true
+set :puma_worker_timeout, nil
+set :puma_init_active_record, false
+ 
+# Defaults:
+set :format,        :pretty
+set :log_level,     :debug
+set :keep_releases, 2
+ 
+## Linked Files & Directories (Default None):
+set :linked_dirs,  %w{ log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system }
+ 
+namespace :puma do
+  desc 'Create Directories for Puma Pids and Socket'
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
+    end
+  end
+ 
+  before :start, :make_dirs
+end
+
+desc 'Invoke a rake command on the remote server'
+task :invoke, [:command] do |task, args|
+  on primary(:app) do
+    within current_path do
+      with :rails_env => fetch(:rails_env) do
+        rake args[:command]
+      end
+    end
+  end
+end
+
+ 
+namespace :deploy do
+  desc "Make sure local git is in sync with remote."
+  task :check_revision do
+    on roles(:app) do
+      unless `git rev-parse HEAD` == `git rev-parse origin/#{fetch(:branch)}`
+        puts "WARNING: HEAD is not the same as origin/#{fetch(:branch)}"
+        puts "Run `git push` to sync changes."
+        exit
+      end
+    end
+  end
+ 
+  desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
+    end
+  end
+ 
+  before :starting,     :check_revision
+  after  :finishing,    :cleanup
+end
+
+
+ 
+# ps aux | grep puma    # Get puma pid
+# kill -s SIGUSR2 pid   # Restart puma
+# kill -s SIGTERM pid   # Stop puma
