ba5a338e54e00298cf22d606e854979ed4644320
Ulysses E. Legaspi <ulysses.legaspi@gmail.com>
2019-07-16 15:20:38 +0800

    Added sidekiq in deploy.rb.

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,7 +1,7 @@
 set :rbenv_type, :user
 set :rbenv_ruby, File.read('.ruby-version').strip
 set :rbenv_prefix, "RBENV_ROOT=#{fetch(:rbenv_path)} RBENV_VERSION=#{fetch(:rbenv_ruby)} #{fetch(:rbenv_path)}/bin/rbenv exec"
-set :rbenv_map_bins, %w{rake gem bundle ruby rails puma pumactl}
+set :rbenv_map_bins, %w{rake gem bundle ruby rails puma pumactl sidekiq sidekiqctl}
 set :rbenv_roles, :all
 
 
