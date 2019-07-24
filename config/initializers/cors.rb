19218bb329299a081cb279a82807611d3466deec
Ulysses E. Legaspi <ulysses.legaspi@gmail.com>
2019-07-24 17:17:55 +0800

    Fix the origin.

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,8 +7,9 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "http://localhost:3000"
-
+#   origins "http://localhost:3000"
+    origins "https://54.251.162.83"
+    
     resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
