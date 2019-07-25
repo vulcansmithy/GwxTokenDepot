a01bac0e948beea141afee8b54561ba5ccee6052
Ulysses E. Legaspi <ulysses.legaspi@gmail.com>
2019-07-25 18:19:20 +0800

    Debugging: Move force_ssl back to application.rb.

--- config/application.rb
+++ config/application.rb
@@ -37,5 +37,7 @@ module GwxTokenDepotApi
     
     config.eager_load_paths << Rails.root.join("lib")
     config.autoload_paths   << Rails.root.join("lib")
+    
+    config.force_ssl = true
   end
 end
