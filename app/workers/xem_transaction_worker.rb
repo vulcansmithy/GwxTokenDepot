6ea152e94cda9902728cac6321eefca0c3263f43
Ulysses E. Legaspi <ulysses.legaspi@gmail.com>
2019-07-26 16:54:08 +0800

    Fix typo.

--- app/workers/xem_transaction_worker.rb
+++ app/workers/xem_transaction_worker.rb
@@ -19,7 +19,7 @@ class XemTransactionWorker
       current_balance = xem_service.check_xem_wallet_balance(xem_transaction)
       
       # convert the quantity_to_receive into a BigDecimal
-      expected_to_receive = BigDecimal(btc_transaction.quantity_to_receive)
+      expected_to_receive = BigDecimal(xem_transaction.quantity_to_receive)
       
       if current_balance == 0
 
