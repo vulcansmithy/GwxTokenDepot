2345c592a44a9e75e370a9bcaecd1d5c22baae07
Ulysses E. Legaspi <ulysses.legaspi@gmail.com>
2019-08-10 13:38:57 +0800

    Updated the TokenDepot prod ip setting.

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,3 +1,3 @@
-server "52.221.192.127", port: 22, roles: [:web, :app, :db], primary: true
+server "13.251.188.204", port: 22, roles: [:web, :app, :db], primary: true
 set :stage, :production
 set :branch, :master
