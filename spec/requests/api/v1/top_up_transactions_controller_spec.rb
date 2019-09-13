c73976536b09d17d07eec1e5914282ec0d4663d9
Ulysses E. Legaspi <ulysses.legaspi@gmail.com>
2019-09-13 15:42:22 +0800

    Enable rspec test that work without having redis running.

--- spec/requests/api/v1/top_up_transactions_controller_spec.rb
+++ spec/requests/api/v1/top_up_transactions_controller_spec.rb
@@ -1,4 +1,7 @@
 require "rails_helper"
+require "sidekiq/testing"
+
+Sidekiq::Testing.fake!
 
 describe Api::V1::TopUpTransactionsController do
 
@@ -48,9 +51,23 @@ describe Api::V1::TopUpTransactionsController do
     
     # make sure the response status was 201 or :created
     expect(response.status).to eq 201  
+    
+    result = JSON.parse(response.body)
+    
+    # make sure the returned 'quantity_to_receive' matches with the request
+    expect(result["data"]["attributes"]["quantity_to_receive"]).to eq "2.0"
+    
+    # make sure the returned 'gwx_to_transfer' matches with the request
+    expect(result["data"]["attributes"]["gwx_to_transfer"]).to eq "30.0"
+
+    # make sure the returned 'top_up_receiving_wallet_address' is not nil
+    expect((result["data"]["attributes"]["top_up_receiving_wallet_address"]).nil?).to eq false
+    
+    # make sure the returned 'status' is set to 'pending'
+    expect(result["data"]["attributes"]["status"]).to eq "pending"
   end
 
-  xit "should be able to successfully call GET /top_up_transactions/calculate/btc/{btc_value}/to_gwx" do
+  it "should be able to successfully call GET /top_up_transactions/calculate/btc/{btc_value}/to_gwx" do
     
     # call the API endpoint
     get "/top_up_transactions/calculate/btc/1.0/to_gwx"
@@ -68,9 +85,17 @@ describe Api::V1::TopUpTransactionsController do
     
     # make sure the gwx has a value
     expect(result["gwx"].nil?).to eq false
+    
+    result = JSON.parse(response.body)
+    
+    # make sure the response has the 'btc' value
+    expect(result["btc"].nil?).to eq false
+    
+    # make sure the response has the 'gwx' value
+    expect(result["gwx"].nil?).to eq false
   end
 
-  xit "should be able to successfully call GET /top_up_transactions/calculate/xem/{xem_value}/to_gwx" do
+  it "should be able to successfully call GET /top_up_transactions/calculate/xem/{xem_value}/to_gwx" do
     
     # call the API endpoint
     get "/top_up_transactions/calculate/xem/10/to_gwx"
@@ -79,6 +104,14 @@ describe Api::V1::TopUpTransactionsController do
     expect(response.status).to eq 200
     
     result = JSON.parse(response.body)
+    
+    puts "@DEBUG L:#{__LINE__}   #{ap result}"
+    
+    # make sure the response has the 'xem' value
+    expect(result["xem"].nil?).to eq false
+    
+    # make sure the response has the 'gwx' value
+    expect(result["gwx"].nil?).to eq false
   end
 
 end
