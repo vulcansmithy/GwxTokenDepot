62bda1e2afed9e174e518481e58207cc571a8317
erson lloyd <elcfrancisco11@gmail.com>
2019-09-17 15:08:02 +0800

    Adds new event in transaction worker

--- app/workers/gwx_transaction_worker.rb
+++ app/workers/gwx_transaction_worker.rb
@@ -5,6 +5,7 @@ class GwxTransactionWorker
   def perform(tx_id)
     transaction = TopUpTransaction.find(tx_id)
     transaction.confirm_gwx_status_from_cashier
+    transaction.validate_gwx_status_from_cashier
   
     if transaction.gwx_transaction_hash.nil?
       puts "Rescheduling worker"
