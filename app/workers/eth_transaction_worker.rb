97420966b2a281a06e3680ec17a069198ab1ecfa
erson lloyd <elcfrancisco11@gmail.com>
2019-09-20 16:15:04 +0800

    Adjusts the validation for Transaction workers.

--- app/workers/eth_transaction_worker.rb
+++ app/workers/eth_transaction_worker.rb
@@ -29,7 +29,7 @@ class EthTransactionWorker
         puts "@DEBUG L:#{__LINE__}   ***************************"
         EthTransactionWorker.perform_in(TopUpTransaction::SCHEDULED_INTERVAL, eth_transaction.id)
 
-      elsif current_balance >= expected_to_receive
+      elsif current_balance > 0 && expected_to_receive > 0
 
         puts "@DEBUG L:#{__LINE__}   ***************************"
         puts "@DEBUG L:#{__LINE__}   *  Transaction Successful!  "
